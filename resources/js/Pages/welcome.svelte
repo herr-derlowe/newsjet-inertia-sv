7b05b0d9476130d9ddd30a318f870781e291bf7b
Reluhcs <126292478+Herr-derLowe@users.noreply.github.com>
2025-03-08 17:38:21 -0400

    project setup finished

---
+++ resources/js/Pages/welcome.svelte
@@ -0,0 +1,10 @@
+<script>
+    let name = $state("World");
+    const NAMES = ["World", "James", "Igor", "Philemon", "Cornelius"];
+    function changeName() {
+        name = NAMES[Math.floor(Math.random() * NAMES.length)];
+    }
+</script>
+
+<h1 class="text-primary">Hello {name}!</h1>
+<button onclick={changeName}>Change name</button>
